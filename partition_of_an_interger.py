cd9058a3b68fb7250daaa406f35dd69d6ae345c7
heliophilia <91010847+tydwg@users.noreply.github.com>
2023-01-08 14:57:37 +0700

    Create partition_of_an_interger.py

---
+++ partition_of_an_interger.py
@@ -0,0 +1,8 @@
+def partition(number):
+    answer = set()
+    answer.add((number, ))
+    for x in range(1, number):
+        for y in partition(number - x):
+            answer.add(tuple(sorted((x, ) + y)))
+    return answer
+print(len(partition(10)))
